75a46f6b57ee4f791a931bb3b6352f8bd251454b
yayak333 <yayak18112000@gmail.com>
2021-03-23 15:41:38 +0700

    menambahkan file index.html dan script.js serta icon gambar

---
+++ script.js
@@ -0,0 +1,13 @@
+h1 = new Typed("#typed", {
+    strings : ["Hallo saya,", "Ahmad Hidayah",],
+    typedSpeed :40,
+    startDelay : 90,
+    loop : true
+});
+
+p = new Typed("#otomatis", {
+    strings : ["Website ini mengenai portofolio saya yang akan saya publik  di internet"],
+    typedSpeed :40,
+    startDelay : 300,
+    loop : true
+});
